557f390d8824dfd7f2c6f6d7bccd04ac8630257d
RahulRingane <rahul897r@gmail.com>
2026-09-09 23:36:49 +0530

    docs: add missing comments to handler.go

--- pkg/http/handler.go
+++ pkg/http/handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// newHandler creates an HTTP handler for the given service that proxies requests to its backends using the provided proxy.
 func newHandler(service *Service, proxy *Proxy) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		backend := service.nextBackend()
